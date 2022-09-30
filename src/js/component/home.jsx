29a5067dc8ca96c720f7bdefcbe7c3e96bbc56f6
h4r0ldru1z <haroldand.ruiz@gmail.com>
2022-09-30 21:55:36 +0000

    tutoría genial OK

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,21 +1,43 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
   const [toDo, setToDo] = useState([]);
   const [tarea, setTarea] = useState("");
 
-  const [mouseover, setMouseover] = useState(false);
+  const [refresh,setRefresh]=useState(true)
+
   const onSubmit = (e) => {
     e.preventDefault();
-    setToDo((prev) => [...prev, tarea]);
+    addArraymetodoPut( [...toDo, {label: tarea, done: false}]); //enviar un array con toda la estructura para mapear
+    //prev son los valores previos del array
     setTarea("");
   };
-  console.log(toDo);
+  //console.log(toDo);
   const elementDelete = (dIndex) => {
-    setToDo(toDo.filter((e, i) => i != dIndex));
+    addArraymetodoPut(toDo.filter((e, i) => i != dIndex));
+    //resultado de un filter es un array, accedemos al array desde 0 hasta i
+    // se guardan todos los elementos en el ToDo excepto los que son iguales al dIndex
   };
 
+  const addArraymetodoPut=(toDo)=>{
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/haroldoruiz", {
+      method: "PUT",
+      body: JSON.stringify(toDo),
+      headers: {
+        "Content-Type" : "application/json"
+      }
+    })
+    .then((resp)=>resp.json()) //respuesta de la API se convierte en JSON
+    .then(()=>setRefresh((prev)=>!prev))//put solo devuleve OK
+  }
+  //método default del fetch es GET
+  useEffect(()=>{
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/haroldoruiz")
+    .then((resp)=>resp.json()) //respuesta de la API se convierte en JSON
+    .then((data)=>setToDo(data))
+  },[refresh])//[] especifica es que se ejecuta una sola vez. useEffect se ejecuta cada vez que la variable cambie
+  //vacío lo ejecuta una sola vez
   return (
     <div className="container">
       <div>
@@ -33,7 +55,7 @@ const Home = () => {
             {toDo.map((element, dIndex) => {
               return (
                 <div className="d-flex justify-content-between my-3 col-4 mx-auto">
-                  <p>{element}</p>
+                  <p>{element.label}</p>
                   <button
                     type="button"
                     className="btn-close"
